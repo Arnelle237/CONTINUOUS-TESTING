a4a456aefcc1884433e99b65c90e4e1333eee43d
Arnelle237 <arnelle.nguefang-tchouamo@edu.dsti.institute>
2024-11-26 20:37:41 +0100

    Update test_password.py

--- test/test_password.py
+++ test/test_password.py
@@ -3,9 +3,9 @@ import pytest
 import io
 # Always run from unit_testing_best_practice/test
 sys.path += ['../src'] 
-from src.password import *
+#from src.password import *
 #since the folder src corresponds now to a branch, we should specify it here before the test
-#from  password import *
+from  password import *
 
 def test_password():
     assert password("abc!/)HUI#123") == "Password is valid!"
